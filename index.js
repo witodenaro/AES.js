088e9a6055fb1ccacb0587a0c7af3930a56d8d33
witodivaro <witodivaro@gmail.com>
2021-11-24 17:56:46 +0300

    add encrypt/decrypt script

---
+++ index.js
@@ -0,0 +1,18 @@
+const fs = require("fs");
+const path = require("path");
+
+const { AES_CBC } = require("./lib/aes_cbc");
+
+const [_, __, key, action = "decrypt"] = process.argv;
+
+if (action !== "encrypt" && action !== "decrypt") throw new Error("Wrong action!");
+
+const inputFileName = action === "encrypt" ? "decrypted.txt" : "encrypted.txt";
+const inputPath = path.resolve(__dirname, "files", inputFileName);
+const input = fs.readFileSync(inputPath, { encoding: "utf-8" });
+
+const output = AES_CBC[action](key, input);
+const outputFileName = action === "encrypt" ? "encrypted.txt" : "decrypted.txt";
+const outputPath = path.resolve(__dirname, "files", outputFileName);
+
+fs.writeFileSync(outputPath, output);
